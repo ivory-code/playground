165e83f030504e50a3652f7c9aa5eecd8870df7e
HanSungKwon <kyo3553@gmail.com>
2022-06-06 16:50:47 +0900

    feat: add redux-persist package

--- App.tsx
+++ App.tsx
@@ -4,8 +4,10 @@ import {createStackNavigator} from '@react-navigation/stack'
 import React from 'react'
 import CalendarPage from './src/components/pages/CalendarPage'
 import CounterPage from './src/components/pages/CounterPage'
-import {store} from './src/reducers/index'
+import {persistor, store} from './src/reducers/index'
 import {Provider} from 'react-redux'
+import {PersistGate} from 'redux-persist/integration/react'
+import {SafeAreaProvider} from 'react-native-safe-area-context'
 
 export type StackParamList = {
   MainPage: undefined
@@ -18,31 +20,35 @@ const Stack = createStackNavigator<StackParamList>()
 const App = () => {
   return (
     <Provider store={store}>
-      <NavigationContainer>
-        <Stack.Navigator initialRouteName="MainPage">
-          <Stack.Screen
-            name="MainPage"
-            component={MainPage}
-            options={{
-              title: 'PlayGround',
-            }}
-          />
-          <Stack.Screen
-            name="CalendarPage"
-            component={CalendarPage}
-            options={{
-              title: 'CalendarPage',
-            }}
-          />
-          <Stack.Screen
-            name="CounterPage"
-            component={CounterPage}
-            options={{
-              title: 'CounterPage',
-            }}
-          />
-        </Stack.Navigator>
-      </NavigationContainer>
+      <PersistGate loading={null} persistor={persistor}>
+        <SafeAreaProvider>
+          <NavigationContainer>
+            <Stack.Navigator initialRouteName="MainPage">
+              <Stack.Screen
+                name="MainPage"
+                component={MainPage}
+                options={{
+                  title: 'PlayGround',
+                }}
+              />
+              <Stack.Screen
+                name="CalendarPage"
+                component={CalendarPage}
+                options={{
+                  title: 'CalendarPage',
+                }}
+              />
+              <Stack.Screen
+                name="CounterPage"
+                component={CounterPage}
+                options={{
+                  title: 'CounterPage',
+                }}
+              />
+            </Stack.Navigator>
+          </NavigationContainer>
+        </SafeAreaProvider>
+      </PersistGate>
     </Provider>
   )
 }
--- src/reducers/index.ts
+++ src/reducers/index.ts
@@ -1,13 +1,25 @@
+import AsyncStorage from '@react-native-async-storage/async-storage'
 import {configureStore} from '@reduxjs/toolkit'
 import {combineReducers} from 'redux'
+import {persistReducer, persistStore} from 'redux-persist'
 import counter from '../stores/counter'
+
+const persistConfig = {
+  key: 'root',
+  storage: AsyncStorage,
+  whiteList: ['auth'],
+}
 
 const reducers = combineReducers({
   counter: counter,
 })
 
+const persistedReducer = persistReducer(persistConfig, reducers)
+
 export const store = configureStore({
-  reducer: reducers,
+  reducer: persistedReducer,
 })
 
 export type RootState = ReturnType<typeof reducers>
+
+export const persistor = persistStore(store)
