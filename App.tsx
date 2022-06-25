a4102f0bf1a3fd2c49f47ff02ce76eb2cf529c86
HanSungKwon <kyo3553@gmail.com>
2022-06-25 17:39:40 +0900

    refactor: call fetchConfig function in [App]

--- App.tsx
+++ App.tsx
@@ -3,13 +3,15 @@ import {createStackNavigator} from '@react-navigation/stack'
 import React from 'react'
 import CalendarPage from './src/components/pages/CalendarPage'
 import CounterPage from './src/components/pages/CounterPage'
-import FirebasePage from './src/components/pages/FirebasePage'
 import MainPage from './src/components/pages/MainPage'
 import {persistor, store} from './src/reducers/index'
 import {Provider} from 'react-redux'
 import {PersistGate} from 'redux-persist/integration/react'
 import {SafeAreaProvider} from 'react-native-safe-area-context'
 import FirebaseNavigator from './src/navigator/FirebaseNavigator'
+import {fetchConfig} from './src/utils/firebase'
+
+fetchConfig().catch(console.log)
 
 export type StackParamList = {
   MainPage: undefined
