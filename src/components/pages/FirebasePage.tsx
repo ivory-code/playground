8984cfa212342d4f1db211d6748c5fe63761b2ec
HanSungKwon <kyo3553@gmail.com>
2022-06-25 16:23:42 +0900

    feat: feat: add modal button

--- src/components/pages/FirebasePage.tsx
+++ src/components/pages/FirebasePage.tsx
@@ -1,7 +1,13 @@
 import {useNavigation} from '@react-navigation/native'
 import {StackNavigationProp} from '@react-navigation/stack'
 import React from 'react'
-import {ScrollView} from 'react-native'
+import {
+  ScrollView,
+  StyleSheet,
+  Text,
+  TouchableOpacity,
+  View,
+} from 'react-native'
 import Link from '../atoms/Link'
 import RefreshConfig from '../atoms/RefreshButton'
 import {FirebaseNavigatorParamList} from '../../navigator/FirebaseNavigator'
@@ -27,8 +33,31 @@ const FirebasePage = () => {
         text="Test Number"
         press={() => navigation.navigate('TestNumber')}
       />
+      <TouchableOpacity style={styles.button}>
+        <Text style={styles.text}>Test Modal</Text>
+      </TouchableOpacity>
     </ScrollView>
   )
 }
 
 export default FirebasePage
+
+const styles = StyleSheet.create({
+  button: {
+    margin: 12,
+    borderWidth: 2,
+    borderColor: '#333',
+    paddingVertical: 8,
+    paddingHorizontal: 16,
+    borderRadius: 32,
+    alignSelf: 'center',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  text: {
+    fontSize: 18,
+    lineHeight: 24,
+    fontWeight: '700',
+    color: '#333',
+  },
+})
